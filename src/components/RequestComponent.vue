724d4b342014cdafa5702611615ade383148e6d3
Nisha Yadav <nishay@mkcl.org>
2021-09-06 08:58:35 +0530

    first commit

---
+++ src/components/RequestComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sl.No</th>
+      <th scope="col">Source Link</th>
+      <th scope="col">Time Stamp</th>
+      <th scope="col">Action</th>
+      <th scope="col">One Time Password</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>//10.15.21.13/source/03092021/BAMU/website</td>
+      <td>Otto</td>
+      <td><button class="btn btn-dark btn-sm">Request Approval</button></td>
+      <td><input type="text" maxlength="4" name="" id=""></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td><button class="btn btn-dark btn-sm">Request Approval</button></td>
+      <td><input type="text" maxlength="4" name="" id=""></td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RequestComponent",
+};
+</script>
+
+<style scoped>
+.table{
+
+}
+</style>
